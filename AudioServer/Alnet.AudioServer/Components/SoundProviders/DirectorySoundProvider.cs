a61f357088f9c100d8ec630605f71fd67d0de5ed
alexander <alexander@656fc658-a34e-3a48-9baf-753936ccefee>
2014-10-04 17:46:09 +0000

    git-svn-id: http://svn.1gb.ru/audioserver/CODE/trunk@10 656fc658-a34e-3a48-9baf-753936ccefee

---
+++ AudioServer/Alnet.AudioServer/Components/SoundProviders/DirectorySoundProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Linq;
+using Alnet.AudioServer.Components.AudioPlayer;
+
+namespace Alnet.AudioServer.Components.SoundProviders
+{
+    class DirectorySoundProvider : ISoundProvider
+    {
+        private readonly string _directoryPath;
+        private SoundInfo[] _sounds;
+
+        public DirectorySoundProvider(string directoryPath)
+        {
+            _directoryPath = directoryPath;
+            _sounds = Directory.GetFiles(_directoryPath)
+                .Select(filePath => new FileInfo(filePath))
+                .Where(f => f.Extension.ToUpper() == ".MP3")
+                .Select(f => new SoundInfo()
+                {
+                    Name = f.Name,
+                    Url = f.FullName
+                })
+                .ToArray();
+        }
+
+        public SoundInfo[] GetSoundList()
+        {
+            return _sounds;
+        }
+
+        public byte[] GetSoundData(int index)
+        {
+            return File.ReadAllBytes(_sounds[index].Url);
+        }
+    }
+}
